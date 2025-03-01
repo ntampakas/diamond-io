cdcb1ba572aa39b727db24577c1bf92527368645
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2025-03-01 16:05:08 +0800

    feat: fix trapdoor

--- src/poly/dcrt/sampler/trapdoor.rs
+++ src/poly/dcrt/sampler/trapdoor.rs
@@ -11,8 +11,14 @@ use crate::{
 
 use openfhe::{
     cxx::UniquePtr,
-    ffi::{DCRTPolyTrapdoorGen, RLWETrapdoorPair},
+    ffi::{DCRTPolyTrapdoorGen, DCRTTrapdoorImpl, RLWETrapdoorPair},
 };
+
+pub struct DCRTPolyTrapdoor {
+    _trapdoor_output: Arc<UniquePtr<DCRTTrapdoorImpl>>,
+    _trapdoor_pair: Arc<UniquePtr<RLWETrapdoorPair>>,
+    _matrix: DCRTPolyMatrix,
+}
 
 pub struct DCRTPolyTrapdoorSampler {
     params: DCRTPolyParams,
@@ -27,23 +33,30 @@ impl DCRTPolyTrapdoorSampler {
 
 impl PolyTrapdoorSampler for DCRTPolyTrapdoorSampler {
     type M = DCRTPolyMatrix;
-    type Trapdoor = Arc<UniquePtr<RLWETrapdoorPair>>;
+    type Trapdoor = DCRTPolyTrapdoor;
 
     fn trapdoor(&self) -> (Self::Trapdoor, Self::M) {
-        let output = DCRTPolyTrapdoorGen(self.params.get_params(), self.base as i64, false);
-        let trapdoor = output.GetTrapdoorPtr();
+        let trapdoor_output =
+            DCRTPolyTrapdoorGen(self.params.get_params(), self.base as i64, false);
+        let trapdoor_pair = trapdoor_output.GetTrapdoorPtr();
         let ncol = ceil_log2(&self.params.modulus()) + 2;
 
         let mut matrix_inner = Vec::with_capacity(1);
         let mut row = Vec::with_capacity(ncol);
         for i in 0..ncol {
-            let poly = output.GetPolyAtIndex(i);
+            let poly = trapdoor_output.GetPolyAtIndex(i);
             let dcrt_poly = DCRTPoly::new(poly);
             row.push(dcrt_poly);
         }
         matrix_inner.push(row);
         let row_matrix = DCRTPolyMatrix::from_poly_vec(&self.params, matrix_inner);
-        (trapdoor.into(), row_matrix)
+        let trapdoor = DCRTPolyTrapdoor {
+            _trapdoor_output: trapdoor_output.into(),
+            _trapdoor_pair: trapdoor_pair.into(),
+            _matrix: row_matrix.clone(),
+        };
+
+        (trapdoor, row_matrix)
     }
 
     fn preimage(&self, _trapdoor: &Self::Trapdoor, _target: &Self::M, _sigma: f64) -> Self::M {
@@ -76,9 +89,7 @@ mod tests {
         let base = 2;
         let sampler = DCRTPolyTrapdoorSampler::new(params, base);
 
-        let (trapdoor, public_matrix) = sampler.trapdoor();
-
-        assert!(!trapdoor.is_null());
+        let (_trapdoor, public_matrix) = sampler.trapdoor();
 
         let expected_cols = ceil_log2(&sampler.params.modulus()) + 2;
 
@@ -96,27 +107,6 @@ mod tests {
                 let poly = public_matrix.entry(i, j);
                 assert!(!poly.get_poly().is_null(), "Matrix entry should be a valid DCRTPoly");
             }
-        }
-    }
-
-    #[test]
-    fn test_trapdoor_with_different_bases() {
-        // Test with different base values
-        let params = DCRTPolyParams::new(16, 4, 51);
-
-        for base in [2, 3, 4, 8] {
-            let sampler = DCRTPolyTrapdoorSampler { params: params.clone(), base };
-            let (trapdoor, public_matrix) = sampler.trapdoor();
-
-            // Check that the trapdoor is not null
-            assert!(!trapdoor.is_null());
-
-            // Calculate expected dimensions
-            let expected_cols = ceil_log2(&params.modulus()) + 2;
-
-            // Check dimensions of the public matrix
-            assert_eq!(public_matrix.row_size(), 1);
-            assert_eq!(public_matrix.col_size(), expected_cols);
         }
     }
 }
