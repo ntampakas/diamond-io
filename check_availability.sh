45d82d0d9e3bf10a52a93304bda11bd9c0cac546
Nick Tampakas <nick@gmx.co.uk>
2025-05-08 12:02:45 +0300

    Check instance availability

---
+++ check_availability.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Script to check if an EC2 instance type is available in a subnet's availability zone
+
+REGION="us-west-2"
+INSTANCE_TYPE="i4i.16xlarge"
+VPC_ID="vpc-085ffb1026b00654e"
+SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=${VPC_ID}" "Name=tag:Type,Values=private" --query "Subnets[].SubnetId" --output json | jq -r '.[]')
+SELECTED_SUBNET=""
+
+[ -z "$SUBNET_IDS" ] && { echo "Error: No private subnets found in VPC $VPC_ID"; exit 1; }
+
+for SUBNET_ID in $SUBNET_IDS; do
+  AZ=$(aws ec2 describe-subnets --subnet-ids "$SUBNET_ID" --region "$REGION" --query "Subnets[0].AvailabilityZone" --output text 2>/dev/null)
+  [ -z "$AZ" ] && continue
+
+  IS_AVAILABLE=$(aws ec2 describe-instance-type-offerings --region "$REGION" --location-type availability-zone --filters Name=location,Values="$AZ" Name=instance-type,Values="$INSTANCE_TYPE" --query "InstanceTypeOfferings[].InstanceType" --output text 2>/dev/null)
+  [ -n "$IS_AVAILABLE" ] && { SELECTED_SUBNET="$SUBNET_ID"; break; }
+done
+
+if [ -n "$SELECTED_SUBNET" ]; then
+  echo "$SELECTED_SUBNET"
+else
+  echo "Error: No subnet supports $INSTANCE_TYPE"
+  exit 1
+fi
--- run.sh
+++ run.sh
@@ -5,18 +5,20 @@ set -eux
 EXIT_CODE=0
 RUNNER_TAG=$1
 
+[ "$INSTANCE_TYPE" = "i4i.16xlarge" ] && SUBNET=$(./check_availability.sh)
+
 cat cloud-init.sh | sed -e "s#__GH_REPO__#${GH_REPO}#" -e "s/__GH_PAT__/${GH_PAT}/" -e "s/__RUNNER_TAG__/${RUNNER_TAG}/" > .startup.sh
 
 INSTANCE_ID=$(aws ec2 run-instances \
   --image-id ${IMAGE_ID} \
-  --block-device-mapping "[ { \"DeviceName\": \"/dev/sda1\", \"Ebs\": { \"VolumeSize\": 16, \"DeleteOnTermination\": true } } ]" \
+  --block-device-mapping "[ { \"DeviceName\": \"/dev/sda1\", \"Ebs\": { \"VolumeSize\": 100, \"DeleteOnTermination\": true } } ]" \
   --ebs-optimized \
   --instance-initiated-shutdown-behavior terminate \
   --instance-type ${INSTANCE_TYPE} \
   --key-name devopsoregon \
   --security-group-ids ${SG} \
   --subnet-id ${SUBNET} \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=drill-${RUNNER_TAG}},{Key=ProjectName,Value=devops}]" "ResourceType=volume,Tags=[{Key=ProjectName,Value=devops}]" \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=machina-io-ephemeral-${RUNNER_TAG}},{Key=ProjectName,Value=machina-io}]" "ResourceType=volume,Tags=[{Key=ProjectName,Value=machina-io}]" \
   --user-data "file://.startup.sh" \
   --query "Instances[0].InstanceId" \
   --output text) || { EXIT_CODE=1; exit $EXIT_CODE; }
