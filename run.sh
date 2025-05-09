4821aeed067b39007f6e6da13bc5820a1e7639dd
Nick Tampakas <nick@gmx.co.uk>
2025-05-09 10:34:13 +0300

    Dev#12

--- run.sh
+++ run.sh
@@ -5,10 +5,8 @@ set -eux
 EXIT_CODE=0
 RUNNER_TAG=$1
 
-#VPC_ID="vpc-085ffb1026b00654e"
-VPC_ID="vpc-00334ffe7c9e3a2b2"
-#SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=${VPC_ID}" "Name=tag:Type,Values=private" --query "Subnets[].SubnetId" --output json | jq -r '.[]')
-SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=${VPC_ID}" --query "Subnets[].SubnetId" --output json | jq -r '.[]')
+VPC_ID="vpc-085ffb1026b00654e"
+SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=${VPC_ID}" "Name=tag:Type,Values=private" --query "Subnets[].SubnetId" --output json | jq -r '.[]')
 
 
 cat cloud-init.sh | sed -e "s#__GH_REPO__#${GH_REPO}#" -e "s/__GH_PAT__/${GH_PAT}/" -e "s/__RUNNER_TAG__/${RUNNER_TAG}/" > .startup.sh
@@ -20,7 +18,7 @@ for SUBNET in $SUBNET_IDS; do
     --ebs-optimized \
     --instance-initiated-shutdown-behavior terminate \
     --instance-type ${INSTANCE_TYPE} \
-    --key-name devops \
+    --key-name devopsoregon \
     --security-group-ids ${SG} \
     --subnet-id ${SUBNET} \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=machina-io-ephemeral-${RUNNER_TAG}},{Key=ProjectName,Value=machina-io}]" "ResourceType=volume,Tags=[{Key=ProjectName,Value=machina-io}]" \
