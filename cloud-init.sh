ccf87bfcdb5ba64528c379168db6590144104d7e
Nick Tampakas <nick@gmx.co.uk>
2025-05-17 15:20:09 +0300

    Orig

--- cloud-init.sh
+++ cloud-init.sh
@@ -9,7 +9,7 @@ RUNNER_TAG="__RUNNER_TAG__"
 
 # idle poweroff script
 cat >> /etc/crontab << 'EOF'
-*/1 * * * * root cat /proc/uptime | awk -F ' ' '{ if ($1 < 300) exit 1 }' && (cat /proc/loadavg | awk -F ' ' '{ if ($1 <= .3 && $2 < .3 && $3 < .3) exit 1 }' || poweroff)
+#*/1 * * * * root cat /proc/uptime | awk -F ' ' '{ if ($1 < 300) exit 1 }' && (cat /proc/loadavg | awk -F ' ' '{ if ($1 <= .3 && $2 < .3 && $3 < .3) exit 1 }' || poweroff)
 EOF
 GH_RUNNER_TOKEN=$(curl -s -X POST -H "Authorization: Bearer ${GH_PAT}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${GH_REPO}/actions/runners/registration-token | jq -r '.token')
 
